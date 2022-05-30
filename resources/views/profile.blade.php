10ef8d23502d8806f1dc40ed6e87ea36619e218f
Anittamolchacko <anittamolchacko@mca.ajce.in>
2022-05-30 09:41:13 +0530

    first commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quas a voluptatibus quam repellat consequuntur vero quasi, numquam, accusamus excepturi laboriosam sint qui odit expedita perferendis? Dignissimos, provident optio officiis, earum fugit excepturi quidem veritatis quos rerum reiciendis, deserunt quasi. Delectus voluptatibus illum ut non odio neque, quasi expedita unde id maiores magnam facilis porro debitis omnis reiciendis rem a ducimus. Ratione delectus, iste, eos, sunt cumque laborum quas dolor corporis sit a vero deserunt soluta iusto dicta minima atque consequuntur aliquam unde modi ipsum. Perferendis obcaecati quidem culpa sint inventore laborum numquam, at, ducimus non aperiam dolor ratione doloremque.</p>
+<img src="{{asset('image/download.jfif')}}" alt="">
+</body>
+</html>
---
+++ resources/views/regform.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body bgcolor="yellow">
+    
+    <form action="" method="get">  
+        
+       <center>
+        <i><h1>REGISTRATION FORM</h1></i><frameset>
+        <table>  
+            
+        <tr>  
+            <td ><H3><label>Enter name:</H3></label></td>   
+            <td><input type="text" ><br> </td>  
+           
+        </tr> 
+        
+        <tr>  
+            <td ><H3><label>Enter password:</H3></label></td>  
+            <td><input type="password" name="password" ><br> </td>  
+        </tr>  
+        <tr>  
+            <td ><H3><label >Enter Email:</H3></label></td>  
+            <td  
+        ><input type="email" ></td>  
+        </tr>  
+        <tr>  
+            <td ><H3><label >Enter Gender:</H3></label></td>  
+            <td>  
+        <input type="radio" name="gender" value="male"/>  
+        <label for="register_gendermale">male</label>  
+        <input type="radio" name="gender"  value="female"/>  
+        <label >female</label>  
+            </td>  
+        </tr>  
+        <tr>  
+            <td class="tdLabel"><H3><label ">Select Country:</H3></label></td>  
+            <td><select name="country" id="register_country" style="width:160px">  
+                <option >--select--</option> 
+            <option >india</option>  
+            <option >pakistan</option>  
+            <option>africa</option>  
+            <option >china</option>  
+            <option >other</option>  
+        </select>  
+        </td>  
+        </tr>  
+        <tr> 
+            <td>
+                <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit"  value="REGISTER"/>  
+        </div></td>  
+        </tr>  
+        </table>  
+        </form>  
+    </center>
+</body>
+</html>
